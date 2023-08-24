9eea72ff383780d324d148f2b09d98ac43f48a9d
Jimmy Ah Fat <j.ahfat95@gmail.com>
2023-08-24 16:16:23 +0100

    oops

--- src/vhdlstuff.cpp
+++ src/vhdlstuff.cpp
@@ -66,6 +66,7 @@ int main(int argc, char** argv)
         {
             lsp::replay connection(args::get(r));
             things::language server(&connection);
+            server.run();
 
             connection.print_status();
         }
